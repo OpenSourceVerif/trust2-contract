3890d2888765d1ea1b870c8e41d33d295c3bb936
temp6 <temp6666666@outlook.com>
2025-10-02 03:45:35 +0800

    cargo-verify: basic wrapper

---
+++ cargo-verify/src/main.rs
@@ -0,0 +1,27 @@
+use std::env;
+use std::path::Path;
+use std::process::{Command, ExitCode};
+
+fn main() -> ExitCode {
+    let mut args = env::args_os();
+    let driver_path = Path::new(&args.next().unwrap())
+        .parent()
+        .unwrap()
+        .join("verify-driver");
+    Command::new(env::var_os("CARGO").unwrap())
+        .arg("build")
+        .args(args.skip(1))
+        .env("RUSTC_WORKSPACE_WRAPPER", driver_path)
+        .status()
+        .map_or_else(
+            |err| {
+                eprintln!("{err}");
+                ExitCode::FAILURE
+            },
+            |status| {
+                status
+                    .code()
+                    .map_or(ExitCode::FAILURE, |code| (code as u8).into())
+            },
+        )
+}
