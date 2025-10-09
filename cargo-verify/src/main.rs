8bdfba1646a6284b5f910e20124ee307681cb22e
temp6 <temp6666666@outlook.com>
2025-10-10 03:37:03 +0800

    integrate Charon

--- cargo-verify/src/main.rs
+++ cargo-verify/src/main.rs
@@ -1,29 +1,8 @@
-use std::{
-    env,
-    path::Path,
-    process::{Command, ExitCode},
-};
+use anyhow::Result;
 
-fn main() -> ExitCode {
-    let mut args = env::args_os();
-    let driver_path = Path::new(&args.next().unwrap())
-        .parent()
-        .unwrap()
-        .join("verify-driver");
-    Command::new(env::var_os("CARGO").unwrap())
-        .arg("build")
-        .args(args.skip(1))
-        .env("RUSTC_WORKSPACE_WRAPPER", driver_path)
-        .status()
-        .map_or_else(
-            |err| {
-                eprintln!("{err}");
-                ExitCode::FAILURE
-            },
-            |status| {
-                status
-                    .code()
-                    .map_or(ExitCode::FAILURE, |code| (code as u8).into())
-            },
-        )
+fn main() -> Result<()> {
+    // --monomorphize?
+    // --no-ops-to-function-calls?
+    // --raw-boxes?
+    charon::main_(["charon", "cargo", "--translate-all-methods"])
 }
