35ad62cdff31d3f5d273570cd3203f73b2def2c3
temp6 <temp6666666@outlook.com>
2026-03-23 21:56:00 +0800

    fix build.sh, pre-commit, tasks.json

--- build.sh
+++ build.sh
@@ -4,8 +4,15 @@ set -e
 
 cargo build -p cargo-verify --release
 
+# Otherwise, dune reports broken symbolic links.
+{
+    mkdir -p charon/charon/target/doc/charon
+    touch -a charon/charon/target/doc/charon/index.html
+    mkdir -p charon/_build/default/_doc/_html/charon
+    touch -a charon/_build/default/_doc/_html/charon/index.html
+}
 (
     cd trust2-contract-verifier
     dune pkg lock
-    dune build -p trust2-contract-verifier @install
+    dune build --release
 )
