ca1732e7bfb1b81b5e01d202aa70682090e47368
temp6 <temp6666666@outlook.com>
2026-05-19 03:54:20 +0800

    basics

--- cargo-verify/src/rust_to_llbc.rs
+++ cargo-verify/src/rust_to_llbc.rs
@@ -2,7 +2,7 @@ use charon_lib::ast::TranslatedCrate;
 use proc_macro_crate::FoundCrate;
 use utils::cargo;
 
-use anyhow::{Context, Result, bail};
+use anyhow::{Result, bail};
 
 use std::{
     collections::HashMap,
@@ -12,7 +12,7 @@ use std::{
 };
 
 pub fn translate_crates(
-    charon_out_dir: Option<PathBuf>,
+    charon_out_dir: PathBuf,
     cargo_build_args: Vec<OsString>,
 ) -> Result<HashMap<String, TranslatedCrate>> {
     let mut crates = HashMap::new();
@@ -28,49 +28,40 @@ pub fn translate_crates(
         }
     };
 
-    let with_charon_out_dir = |charon_out_dir: PathBuf| {
-        charon::main_(
-            [
-                "charon".into(),
-                "cargo".into(),
-                "--translate-all-methods".into(),
-                "--hide-marker-traits".into(),
-                "--hide-allocator".into(),
-                "--treat-box-as-builtin".into(),
-                "--reconstruct-asserts".into(),
-                "--dest".into(),
-                charon_out_dir.clone().into(),
-                "--".into(),
-            ]
-            .into_iter()
-            .chain(cargo_build_args)
-            .chain(feature_args),
-        )?;
+    charon::main_(
+        [
+            "charon".into(),
+            "cargo".into(),
+            // Do not enable:
+            // --unbind-item-vars
+            "--abort-on-error".into(),
+            "--translate-all-methods".into(),
+            "--monomorphize".into(),
+            "--treat-box-as-builtin".into(),
+            "--precise-drops".into(),
+            // "--desugar-drops".into(),
+            "--index-to-function-calls".into(),
+            "--ops-to-function-calls".into(),
+            "--reconstruct-asserts".into(),
+            "--hide-allocator".into(),
+            "--hide-marker-traits".into(),
+            "--dest".into(),
+            charon_out_dir.clone().into(),
+            "--".into(),
+        ]
+        .into_iter()
+        .chain(cargo_build_args)
+        .chain(feature_args),
+    )?;
 
-        for dir_entry in fs::read_dir(&charon_out_dir)? {
-            let llbc_file = dir_entry?.path();
-            if llbc_file.is_file() && llbc_file.extension() == Some(OsStr::new("llbc")) {
-                let crate_ = charon_lib::deserialize_llbc(&llbc_file)?;
-                if let Some(crate_) = crates.insert(crate_.crate_name.clone(), crate_) {
-                    bail!("duplicate crate name: {}", crate_.crate_name);
-                }
+    for dir_entry in fs::read_dir(&charon_out_dir)? {
+        let llbc_file = dir_entry?.path();
+        if llbc_file.is_file() && llbc_file.extension() == Some(OsStr::new("llbc")) {
+            let crate_ = charon_lib::deserialize_llbc(&llbc_file)?;
+            if let Some(crate_) = crates.insert(crate_.crate_name.clone(), crate_) {
+                bail!("duplicate crate name: {}", crate_.crate_name);
             }
         }
-
-        Ok(())
-    };
-
-    if let Some(charon_out_dir) = charon_out_dir {
-        with_charon_out_dir(charon_out_dir)?;
-    } else {
-        let temp_dir = tempfile::tempdir()?;
-        let result = with_charon_out_dir(temp_dir.path().into());
-        let err_msg = format!(
-            "failed to delete temporary directory: {}",
-            temp_dir.path().display(),
-        );
-        temp_dir.close().context(err_msg)?;
-        result?;
     }
 
     Ok(crates)
